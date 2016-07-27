d153b4dcf9128dedefaddc64ee818b7d5c48405c
Teodor Penkov <bgtspen@gmail.com>
2016-07-27 17:56:40 +0300

    Added models.

---
+++ app/hello_menu.py
@@ -0,0 +1,27 @@
+from model.menu_item import MenuItem
+from model.menu_header import MenuHeader
+
+def printCategory(category, items):
+    print(category.name)
+    print('-------------------')
+    for value in items:
+        print("{} - {}".format(value.name, value.price))
+
+# Create the menu
+soupHeader = MenuHeader('Супи')
+soupItems = []
+soupItems.append(MenuItem('Таратор', '1.00 лв.'))
+soupItems.append(MenuItem('Пилешка Супа', '1.50 лв.'))
+
+mainDishesHeader = MenuHeader('Основно')
+mainDishesItems = []
+mainDishesItems.append(MenuItem('Пиле с ориз', '3.50 лв.'))
+
+# Print the menu
+print("********************")
+print()
+printCategory(soupHeader, soupItems)
+print()
+printCategory(mainDishesHeader, mainDishesItems)
+print()
+print("********************")
---
+++ app/model/Lunch.py
@@ -0,0 +1,3 @@
+class Lunch(object):
+    def __init__(self, ):
+        suite
